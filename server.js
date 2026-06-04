2f7a120500d30987165c7ff6264a0543dc8dcbdf
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2026-06-04 15:09:46 +0530

    update changes

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://archifyandbuildify.netlify.app'
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
